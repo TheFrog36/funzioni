c9c0af3c546727723a520b16b16c163700a77b7b
TheFrog36 <artisangiorgio98@gmail.com>
2022-04-22 13:29:59 +0200

    Update funzioni.js

--- funzioni.js
+++ funzioni.js
@@ -9,9 +9,26 @@ function fizzbuzz(number){
     if(number % 3 === 0) txt += "FIZZ"
     if(number % 5 === 0) txt += 'BUZZ';
     if(number === 0) return;    
-    console.log(txt);
+    return txt;
 }
 
-fizzbuzz(15);
+console.log(fizzbuzz(15));
 
+function check_limit(number){
+    if(number > 10) return 10;
+    return number;
+}
 
+function lower_than_5(number){
+    if(number > 5) return 0;
+    return 1;
+}
+function elabora_dato(dato){
+    const dato_rounded = Math.round(dato);
+    const dato_pow_2 = dato_rounded **2;
+    
+    const number_after_check = check_limit(dato_pow_2);
+    return lower_than_5(number_after_check);
+}
+
+console.log(elabora_dato(1.423))
